d33acd35ec5f365903f346aa0f77241b790a3544
Robert JONES <rojones@e5r10p5.jnb.42.fr>
2016-10-14 14:47:15 +0200

    norm oh norm where are you

--- includes/ex_system.h
+++ includes/ex_system.h
@@ -6,7 +6,7 @@
 /*   By: rojones <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/26 16:55:21 by rojones           #+#    #+#             */
-/*   Updated: 2016/10/14 10:10:03 by arnovan-         ###   ########.fr       */
+/*   Updated: 2016/10/14 14:43:21 by rojones          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,14 @@
 # include <ctype.h>
 
 # define C_RED \e[3m
+# define OR_WARNING "\x1B[31mWarning OR type operand in conclusion, result is \
+undefined\x1B[0m\n"
+# define RULE_USED "Used rule [ \"%s => %s\" ] to solve %c - Result: %d\n"\
+, g_rules[sv->i].condition, g_rules[sv->i].conclusion, fact, re
+# define CONTRADIC_WARNING "\x1B[31mWarning contradicting results for %c - \
+Previous Result: %d, Current Result: %d\x1B[0m\n", fact, sv->final, re
+# define CONDITION_NOT_MET "\x1B[33mcondition [%s] %d not met fact[%c] not \
+changed\n\x1B[0m", g_rules[sv->i].condition, re, fact
 
 typedef struct	s_rule
 {
@@ -26,21 +34,49 @@ typedef struct	s_rule
 	char	*conclusion;
 }				t_rule;
 
-int		g_num_rules;
-int		g_facts[26];
-int		g_default[26];
-int		g_short;
-t_rule	*g_rules;
-char	*g_prove;
+typedef struct	s_solve
+{
+	int		i;
+	int		pos;
+	int		final;
+}				t_solve;
 
-int		ft_and(int t1, int t2);
-int		ft_or(int t1, int t2);
-int		ft_xor(int t1, int t2);
-int		ft_negate(int t);
-int		ft_eval_cond(char *condition, int *solving);
-void	ft_read_file(char *file);
-void	ft_read_info(char *file);
-char	*ft_strnew(size_t size);
-int		ft_solve_for(char fact, int *solving);
-void	ft_validate_rule(int rulenum, int line_no);
+typedef struct	s_eval
+{
+	int     neg1;
+	int     neg2;
+	int     t1;
+	int     t2;
+	int     op;
+	char    *ncon;
+}				t_eval;
+
+typedef struct	s_eval_brack
+{
+	int		st;
+	int		end;
+	int		brace;
+	char	*left;
+	char	*right;
+	char	*temp;
+}				t_eval_brack;
+
+int				g_num_rules;
+int				g_facts[26];
+int				g_default[26];
+int				g_short;
+t_rule			*g_rules;
+char			*g_prove;
+
+int				ft_eval_brackets(char *condition, int *solving);
+int				ft_and(int t1, int t2);
+int				ft_or(int t1, int t2);
+int				ft_xor(int t1, int t2);
+int				ft_negate(int t);
+int				ft_eval_cond(char *condition, int *solving);
+void			ft_read_file(char *file);
+void			ft_read_info(char *file);
+char			*ft_strnew(size_t size);
+int				ft_solve_for(char fact, int *solving);
+void			ft_validate_rule(int rulenum, int line_no);
 #endif
--- srcs/ft_solve_for.c
+++ srcs/ft_solve_for.c
@@ -6,7 +6,7 @@
 /*   By: rojones <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/27 15:43:40 by rojones           #+#    #+#             */
-/*   Updated: 2016/10/14 11:10:57 by arnovan-         ###   ########.fr       */
+/*   Updated: 2016/10/14 14:33:04 by rojones          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 static int	ft_check_rule(char *conclusion, char fact)
 {
-	int	i = -1;
+	int	i;
 
-	while(conclusion[++i] != '\0')
+	i = -1;
+	while (conclusion[++i] != '\0')
 	{
 		if (conclusion[i] == fact)
 			return (i);
@@ -24,117 +25,11 @@ static int	ft_check_rule(char *conclusion, char fact)
 	return (-1);
 }
 
-static int	ft_bracket_length(int start, char *condition)
-{
-	int i;
-	int skip;
-
-	i = 1;
-	skip = 0;
-	while (condition[start + i] != '\0')
-	{
-		if (condition[start + i] == '(')
-			skip++;
-		if (condition[start + i] == ')' && skip == 0)
-			return (i);
-		else if (condition[start + i] == ')')
-			skip--;
-		i++;
-	}
-	return (i);
-}
-
-static int	ft_eval_brackets(char *condition, int *solving)
-{
-	int		st;
-	int		end;
-	int		brace;
-	char	*left;
-	char	*right;
-	char	*temp;
-
-	st = (long)(strchr(condition, '(')) - (long)(condition);
-	end = st + ft_bracket_length(st, condition);
-	left = ft_strnew(st);
-	right = ft_strnew(strlen(condition) - end);
-	temp = ft_strnew(end - st - 1);
-	left = memcpy(left, condition, st);
-	temp = memcpy(temp, &condition[st + 1], end - st - 1);
-	right = memcpy(right, &condition[end + 1], strlen(condition) - end);
-	brace = ft_eval_cond(temp, solving);
-	temp = ft_strnew(strlen(left) + strlen(right) + 1);
-	temp = memcpy(temp, left, strlen(left));
-	temp[strlen(left)] = brace + '0';
-	temp = strcat(temp, right);
-	free(left);
-	free(right);
-	return (ft_eval_cond(temp, solving));
-}
-
-int		ft_eval_cond(char *condition, int *solving)
-{
-	int 	neg1 = 0;
-	int		neg2 = 0;
-	int		t1;
-	int		t2;
-	int		op;
-	char	*ncon;
-
-	op = -2;
-	if (condition[0] != '!' && condition[1] == '\0')
-		op = ((isalpha(condition[0])) ? 
-				ft_solve_for(condition[0], solving) : condition[0] - '0');
-	else if (condition[0] == '!' && condition[2] == '\0')
-	{
-		op = ((isalpha(condition[1])) ? ft_solve_for(condition[1], solving)
-				: condition[1] - '0');
-		op = ft_negate(op);
-	}
-	else
-	{
-		if (strchr(condition, '(') == NULL)
-		{
-			neg1 = (condition[0] == '!') ? 1 : 0;
-			neg2 = (condition[2 + neg1] == '!')?1 : 0;
-			t1 =(isalpha(condition[0 + neg1]))? ft_solve_for(condition[0 +
-					neg1], solving) : condition[0 + neg1] - '0';
-			t2 =(isalpha(condition[2 + neg1 + neg2]))? ft_solve_for(condition[2
-					+ neg1 + neg2], solving) : condition[2 + neg1 + neg2] - '0';
-			if (neg1)
-				t1 = ft_negate(t1);
-			if (neg2)
-				t2 = ft_negate(t2);
-			if (t1 == -2 || t2 == -2)
-				return (-2);
-			if (condition[1 + neg1] == '+')
-					op = ft_and(t1, t2);
-			else if (condition[1 + neg1] == '|')
-					op = ft_or(t1, t2);
-			else if (condition[1 + neg1] == '^')
-					op = ft_xor(t1, t2);
-			if (condition[3 + neg1 + neg2] == '\0')
-				return (op);
-			else
-			{
-				ncon = ft_strnew(1 + (strlen(&condition[3 + neg1 + neg2])));
-				ncon[0] = '0' + op;
-				ncon = strcat(ncon, &condition[3 + neg1 + neg2]);
-				op = ft_eval_cond(ncon, solving);
-			}
-		}
-		else
-		{
-			op = ft_eval_brackets(condition, solving);
-		}
-	}
-	free (condition);
-	return op;
-}
-
 static int	solving_term(char *condition, int *solving)
 {
-	int i = 0;
-
+	int	i;
+	
+	i = 0;
 	while (condition[i] != '\0')
 	{
 		if (isupper(condition[i]) && solving[condition[i] - 'A'] == 1)
@@ -144,64 +39,72 @@ static int	solving_term(char *condition, int *solving)
 	return (0);
 }
 
-int	ft_solve_for(char fact, int *solving)
+void		ft_run_check(int *solving, t_solve *sv, char fact)
 {
-	int i = 0;
-	int pos;
-	int	final = -1;
-	int re;
+	int	re;
 
-	if (g_short == 0 || (g_facts[fact -'A'] == -1 && g_short == 1))
+	if ((re = ft_eval_cond(strdup(g_rules[sv->i].condition), solving)) == 1)
 	{
-		solving[fact - 'A'] = 1;
-		while (i < g_num_rules)
+		if (strchr(g_rules[sv->i].conclusion, '|') ||
+				strchr(g_rules[sv->i].conclusion, '^'))
 		{
-			if (solving_term(g_rules[i].condition, solving) == 0 && (pos =
-						ft_check_rule(g_rules[i].conclusion, fact)) != -1)
-			{
-				if (strchr(g_rules[i].conclusion, '|') ||
-						strchr(g_rules[i].conclusion, '^'))
-				{
-					printf("\x1B[31mWarning OR type operand in conclusion, result is undefined\x1B[0m\n");
-					re = -2;
-				}
-				else
-				{
-					re = ft_eval_cond(strdup(g_rules[i].condition), solving);
-					if (pos != 0)
-					{
-						if (g_rules[i].conclusion[pos - 1] == '!')
-							re = ft_negate(re);
-					}
-				}
-				printf("Used rule [ \"%s => %s\" ] to solve %c - Result: %d\n", g_rules[i].condition, g_rules[i].conclusion, fact, re);
-				if (final == -1)
-					final = re;
-				else if (re != final && re != -2 && final != -2)
-					printf("\x1B[31mWarning contradicting results for %c - Previous Result: %d, Current Result: %d\x1B[0m\n", fact, final, re);
-				else if (re != -2)
-					final = re;
-			}
-			i++;
+			printf(OR_WARNING);
+			re = -2;
 		}
-		solving[fact - 'A'] = 0;
-		if (final == -1)
-		{
-			printf("Solving %c from default value %d\n", 
-					fact, g_default[fact - 65]);
-			return (g_default[fact - 65]);
-		}
-		else
-		{
-			printf("Collective result of %c : %d\n", fact, final);
-			g_facts[fact - 'A'] = final;
-			return (final);
-		}
+		if (sv->pos != 0 && re != -2)
+			if (g_rules[sv->i].conclusion[sv->pos - 1] == '!')
+				re = ft_negate(re);
+		printf(RULE_USED);
+		if (sv->final == -1)
+			sv->final = re;
+		else if (re != sv->final && re != -2 && sv->final != -2)
+			printf(CONTRADIC_WARNING);
+		else if (re != -2)
+			sv->final = re;
+	}
+	else
+		printf(CONDITION_NOT_MET);
+}
+
+int			ft_find_solution(char fact, int *solving)
+{
+	t_solve sv;
+
+	sv.i = 0;
+	sv.final = -1;
+	solving[fact - 'A'] = 1;
+	while (sv.i < g_num_rules)
+	{
+		if (solving_term(g_rules[sv.i].condition, solving) == 0 && (sv.pos =
+					ft_check_rule(g_rules[sv.i].conclusion, fact)) != -1)
+			ft_run_check(solving, &sv, fact);
+		sv.i++;
+	}
+	solving[fact - 'A'] = 0;
+	if (sv.final == -1)
+	{
+		printf("Solving %c from default value %d\n",
+				fact, g_default[fact - 65]);
+		return (g_default[fact - 65]);
 	}
 	else
 	{
-			printf("\x1B[33mUsing previous result of \
-					%c: %d\n\x1B[0m", fact, g_facts[fact - 'A']);
-			return (g_facts[fact - 'A']);
+		printf("Collective result of %c : %d\n", fact, sv.final);
+		g_facts[fact - 'A'] = sv.final;
+		return (sv.final);
+	}
+}
+
+int			ft_solve_for(char fact, int *solving)
+{
+	if (g_short == 0 || (g_facts[fact -'A'] == -1 && g_short == 1))
+	{
+		return (ft_find_solution(fact, solving));
+	}
+	else
+	{
+		printf("\x1B[33mUsing previous result of \
+				%c: %d\n\x1B[0m", fact, g_facts[fact - 'A']);
+		return (g_facts[fact - 'A']);
 	}
 }
